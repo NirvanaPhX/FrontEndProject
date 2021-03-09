c6935fdbaa1391d33931ec625970f365b840be28
Nirvanaphx <lixiaomengg@gmail.com>
2021-03-08 23:42:49 -0700

    Implemented conversion function, added timestamp, fixed some bugs Now the app is fully functional.

--- currencyconverter/main.js
+++ currencyconverter/main.js
@@ -2,6 +2,7 @@
 var newNamespace = {};
 newNamespace.ArrayOfFromCurrency = [];
 newNamespace.ArrayOfToCurrency = [];
+newNamespace.time = null;
 
 const createCurrency = (id, symbol, selected) => {
 	return {
@@ -12,18 +13,18 @@ const createCurrency = (id, symbol, selected) => {
 }
 
 function findSelected(list) {
-	for (let i=0; i<list.length; i++) {
+	for (let i = 0; i < list.length; i++) {
 		if (list[i].selected) {
 			return list[i];
 		}
 	}
 }
-	
+
 // Validate element id and add it to array
 function addToCurrencyList(id, symbol, selected, list) {
 	let element = document.getElementById(id);
 	if (element && element.innerHTML != undefined) {
-		list.push(createCurrency(id, symbol, selected));	
+		list.push(createCurrency(id, symbol, selected));
 	} else {
 		console.log("Did not find element " + element);
 	}
@@ -45,10 +46,10 @@ function populateCurrencyList() {
 
 function changeToSelected(currency, list, displayId) {
 	if (!currency.selected) {
-		currency.selected = true; 
+		currency.selected = true;
 		document.getElementById(currency.id).style.background = "rgb(10, 20, 110)";
-		document.getElementById(displayId).innerHTML = `<h2>${currency.symbol}</h2>`; 
-		for (let i=0; i<list.length; i++) {
+		document.getElementById(displayId).innerHTML = `<h2>${currency.symbol}</h2>`;
+		for (let i = 0; i < list.length; i++) {
 			if (list[i].id != currency.id) {
 				list[i].selected = false;
 				document.getElementById(list[i].id).style.background = "#0071eb";
@@ -58,16 +59,17 @@ function changeToSelected(currency, list, displayId) {
 }
 
 function addListenerToArray(list, displayId, id1, id2) {
-	for (let i=0; i<list.length; i++) {
+	let rate;
+	for (let i = 0; i < list.length; i++) {
 		document.getElementById(list[i].id).addEventListener("click", event => {
 			changeToSelected(list[i], list, displayId);
 			let element1 = document.getElementById(id1);
 			let element2 = document.getElementById(id2);
-			if (element1 && element2 && (element1.value != 0 || element2.value !=0) ) {
-				let rate = findRate();
+			if (element1 && element2 && (element1.value != 0 || element2.value != 0)) {
+				rate = findRate();
 				element2.value = (element1.value * rate).toFixed(2);
-				
 			}
+			UpdateElement("updaterate", `The rate is <em>${rate}</em>`);
 		})
 	}
 }
@@ -79,55 +81,82 @@ function addListenerToInput(id1, id2) {
 		element1.addEventListener("change", event => {
 			let rate = findRate();
 			element2.value = (element1.value * rate).toFixed(2);
+			var time = new Date(newNamespace.time * 1000);
+			UpdateElement("updatetime", `The rate was updated at <em>${time}</em>`);
+			UpdateElement("updaterate", `The rate is <em>${rate}</em>`);
+			UpdateElement("otherinfo", `The rate is updated every hour.`);
 		})
 	}
 }
 
+function UpdateElement(id, newValue) {
+	var elementOnForm = document.getElementById(id);
+	if (elementOnForm && elementOnForm.innerHTML !== undefined) {
+		elementOnForm.innerHTML = newValue;
+	}
+	else {
+		console.log(`Could not field id '${id}' to update innerHTML\n${newValue}`);
+	}
+}
+
 function findRate() {
-	var currencyFrom = findSelected(newNamespace.ArrayOfFromCurrency); 
-	var currencyTo = findSelected(newNamespace.ArrayOfToCurrency); 
-	
+	var rate = 1.0;
+	var currencyFrom = findSelected(newNamespace.ArrayOfFromCurrency);
+	var currencyTo = findSelected(newNamespace.ArrayOfToCurrency);
+
 	if (currencyFrom.symbol && currencyTo.symbol) {
-		// Fetch the current rates from the following API
-		fetch('https://openexchangerates.org/api/latest.json?app_id=74c88ee76ec040979bc2b7841b5b5813')
-		// Convert data to JSON format
-		.then((resp) => resp.json())
-		// Update rates in money.js to current rates
-		.then((data) => fx.rates = data.rates)
-		// Apply the rate
+		fetchData();
 	}
 
-	return fx(1).from(currencyFrom.symbol).to(currencyTo.symbol);
+	// if 
+	try {
+		return fx(1).from(currencyFrom.symbol).to(currencyTo.symbol);
+	} catch {
+		setUpDefaultRate();
+		return fx(1).from(currencyFrom.symbol).to(currencyTo.symbol);
+	}
+}
+
+function fetchData() {
+	// Fetch data through Open Exchange Rates API
+	fetch('https://openexchangerates.org/api/latest.json?app_id=74c88ee76ec040979bc2b7841b5b5813')
+		//Convert data to JSON format
+		.then((resp) => resp.json())
+		//Update rates in money.js to current rates
+		.then((data) => {
+			fx.rates = data.rates;
+			fx.base = data.base;
+			//Grap timestamp
+			newNamespace.time = data.timestamp;
+		})
+		.catch((reason) => {
+			console.log("Handle rejected promise (${reason}) here.");
+		})
 }
 
 function setUpDefaultRate() {
+
 	fx.rates = {
 		CAD: 1.26541,
 		ETH: 0.00061,
 		BTC: 0.000020207795,
 		USD: 1,
 		EUR: 0.839201
-	}
+	};
 	fx.base = 'USD';
 }
-	
+
 
 
 // Change the background and border of the selected element
-window.onload = () => { 
+window.onload = () => {
 	populateCurrencyList();
-	
-	addListenerToArray(newNamespace.ArrayOfFromCurrency, "FromHidden", "from", "to");
-	addListenerToArray(newNamespace.ArrayOfToCurrency, "ToHidden", "to", "from");
 
+	addListenerToArray(newNamespace.ArrayOfFromCurrency, "FromHidden", "from", "to");
+	addListenerToArray(newNamespace.ArrayOfToCurrency, "ToHidden", "from", "to");
+
+	fetchData();
 	setUpDefaultRate();
 
-	fetch('https://openexchangerates.org/api/latest.json?app_id=74c88ee76ec040979bc2b7841b5b5813')
-	.then((resp) => resp.json())
-	.then((data) => fx.rates = data.rates)
-
-	fx.append('ETH', 0.00061);
-		
 	addListenerToInput("from", "to");
-	addListenerToInput("to", "from");
-}	
+}
